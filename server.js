4936ea5c82e8262052e43e9e2b1b0928c0d3815b
Hazal Ozcan <hazal.ozcan@yahoo.com>
2016-02-15 12:34:41 -0800

    Building up the back end

---
+++ server.js
@@ -0,0 +1,29 @@
+
+//modules ======
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+//configuration ======
+//config files
+var db = require('./config.db');
+
+// set our port
+
+var port = process.env.PORT || 8080;
+
+//parse application/json
+app.use(bodyParser.json());
+
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+app.use(express.static(__dirname + '/public'));
+
+require('./app/routes')(app);
+
+app.listen(port);
+
+console.log("You are connected to the port ");
+
+exports = module.exports = app;
